dceaf1dcf86c47f9b2d4db5fc111078eab66fe31
Christian.perin <Christian.perin@OSA0625213W10-1>
2023-05-10 22:20:16 -0300

    Adicionar arquivos de projeto.

---
+++ WebGeradorMassaDados/Controllers/GerarNomeLista.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebGeradorMassaDados.Regras;
+
+namespace WebGeradorMassaDados.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GerarNomeLista : Controller
+    {
+        [HttpGet(Name = "Gerador Nome Lista")]
+        public List<string> Nome(string sexo, int QtdeNomesGerar)
+        {
+            List<string> ListNome = new List<string>();
+            Nomes nomes = new Nomes();
+            for (int i = 0; i < QtdeNomesGerar; i++)
+            {
+                ListNome.Add(nomes.nome(sexo).Trim().TrimEnd().TrimStart());
+            }
+            return ListNome;
+        }
+    }
+
+}
---
+++ WebGeradorMassaDados/Controllers/ListaPessoas.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebGeradorMassaDados.Regras;
+
+namespace WebGeradorMassaDados.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class ListaPessoas : Controller
+    {
+        [HttpGet(Name = "Gerador ListaPessoas")]
+        public List<WebGeradorMassaDados.Objetos.Pessoa> CriarLista(int IdadeInicial, 
+                    int IdadeFinal , 
+                    int QuantasPessoas)
+        {
+            List<WebGeradorMassaDados.Objetos.Pessoa> pessoas 
+                = new List<WebGeradorMassaDados.Objetos.Pessoa>();
+
+            Fabricar fabricar = new Fabricar();
+            for (int i = 0; i < QuantasPessoas; i++)
+            {
+                pessoas.Add(fabricar.Criar(IdadeInicial, IdadeFinal));
+            }
+            return pessoas; 
+        }
+    }
+}
